18d0eca5ba3cfb8bb392908b592f9111511742f9
Liang YuChao <43328497+KingoftheNight@users.noreply.github.com>
2021-09-25 19:09:38 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(name='irap',
     author_email='1694822092@qq.com',
     license='BSD 2-Clause',
     packages=['irap'],
-    install_requires=['itertools', 'pyecharts'],
+    install_requires=['pyecharts'],
     entry_points={
         'console_scripts': [
         'irap=irap.__main__:irap',
